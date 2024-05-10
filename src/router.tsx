ee829137e7d129e80746f8b2c3d49151e81be866
LucausMartin <mzx2602685411@gmail.com>
2024-05-10 17:15:37 +0800

    feat: ✨ 更新目录结构

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import App from './App.tsx';
-import { Error } from '@myPages/error';
-import { Test } from '@myPages/test';
+import { Error, Test } from '@myPages/index.ts';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(

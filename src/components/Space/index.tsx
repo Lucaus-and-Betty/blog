303664dfd592468db578ebb3de521f89e7d9be30
LucausMartin <84424319+LucausMartin@users.noreply.github.com>
2024-05-14 19:00:03 +0800

    Delete src/components/Space directory

--- src/components/Space/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-const Space: FC<{ width: string; height: string; display?: string }> = ({ width, height, display }) => {
-  return <div style={{ width, height, display }}></div>;
-};
-
-export { Space };

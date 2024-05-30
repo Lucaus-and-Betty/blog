666ff869791c266cf8612fbe77276e54d6753588
LucausMartin <mzx2602685411@gmail.com>
2024-05-30 18:29:50 +0800

    feat: ✨ 完成星星 canvas

--- src/components/starsCanvas/index.tsx
+++ src/components/starsCanvas/index.tsx
@@ -1,66 +1,98 @@
-import { useEffect, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Star, StarConfig } from './type';
 import './index.less';
 
 const StarsCanvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [canvasState, setCanvasState] = useState<ImageData | null>(null);
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [height, setHeight] = useState<number>(window.innerHeight);
 
   useEffect(() => {
-    const stars: StarConfig[] = [];
-    console.log('canvasRef.current');
-    // 获取窗口大小
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    if (canvasRef.current) {
-      canvasRef.current.width = width;
-      canvasRef.current.height = height;
+    console.log('每次都渲染的 useEffect');
+    // 将保存的画布状态恢复
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
     }
-    // 监听窗口大小变化
-    window.addEventListener('resize', () => {
-      if (canvasRef.current) {
-        canvasRef.current.width = window.innerWidth;
-        canvasRef.current.height = window.innerHeight;
-
-        // 重绘星星
-        const ctx = canvasRef.current?.getContext('2d');
-        if (!ctx) {
-          return;
-        }
-        ctx.clearRect(0, 0, width, height);
-        stars.forEach(star => {
-          star.starElement.draw();
-        });
-      }
-    });
-
-    // 获取画布上下文
-    const ctx = canvasRef.current?.getContext('2d');
+    const ctx = canvas.getContext('2d');
     if (!ctx) {
       return;
     }
-    // 隔一段时间就创建一个星星
-    const timer = setInterval(() => {
-      // 不超过 250 个星星
-      if (stars.length > 250) {
-        clearInterval(timer);
-        return;
-      }
-      // 创建星星
-      const star = new Star(ctx, width, height);
-      const starInfo = star.draw();
-      if (starInfo) {
-        stars.push({
-          starInfo,
-          starElement: star
-        });
-      }
-    }, 200);
+
+    if (canvasState) {
+      ctx.putImageData(canvasState, 0, 0);
+    }
+  });
+
+  useEffect(() => {
+    console.log('只渲染一次的 useEffect');
+    // 初始化画布
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      console.error('不支持 canvas');
+      return;
+    }
+
+    // 窗口大小改变时，重新设置画布大小
+    window.onresize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+      setCanvasState(ctx.getImageData(0, 0, window.innerWidth, window.innerHeight));
+    };
+
+    // 初始化 250 个星星
+    const stars: StarConfig[] = [];
+    for (let i = 0; i < 250; i++) {
+      const x = Math.random() * window.innerWidth;
+      const y = Math.random() * window.innerHeight;
+      const scale = Math.random() * 2;
+      const alpha = Math.random();
+      let speed = Math.random() + 0.1;
+      speed = speed > 0.8 ? speed - 0.5 : speed;
+      speed = speed > 0.5 ? speed - 0.4 : speed;
+      // 方向在 0 - 1
+      const direction = Math.random();
+      stars.push({
+        starElement: new Star(x, y, scale, alpha, speed, direction)
+      });
+    }
+    stars.forEach(star => {
+      star.starElement.draw(ctx);
+    });
+
+    // 星星移动
+    const moveStars = () => {
+      ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      stars.forEach(star => {
+        const isOut = star.starElement.move(ctx);
+        if (isOut) {
+          // 新星星只能在左边 3/5 的位置
+          const x = (Math.random() * window.innerWidth) / 2;
+          const y = Math.random() * window.innerHeight;
+          const scale = Math.random() * 2;
+          const alpha = Math.random();
+          let speed = Math.random() + 0.1;
+          speed = speed > 0.5 ? speed - 0.4 : speed;
+          const direction = Math.random();
+          star.starElement = new Star(x, y, scale, alpha, speed, direction);
+          star.starElement.draw(ctx);
+        }
+      });
+    };
+
+    // 每 16.7ms 移动一次
+    const timer = setInterval(moveStars, 16.7);
 
     return () => {
       clearInterval(timer);
     };
-  });
-  return <canvas ref={canvasRef} id="stars"></canvas>;
+  }, []);
+  return <canvas width={width} height={height} ref={canvasRef} id="universe"></canvas>;
 };
 
 export { StarsCanvas };
--- src/components/starsCanvas/type.ts
+++ src/components/starsCanvas/type.ts
@@ -1,72 +1,87 @@
 export interface StarConfig {
-  starInfo: StarInfo;
   starElement: Star;
 }
 
-interface StarInfo {
-  x: number;
-  y: number;
-  scale: number;
-  alpha: number;
-}
+/**
+ * 星星类
+ */
 export class Star {
-  ctx: CanvasRenderingContext2D | null = null;
-  panelWidth: number;
-  panelHeight: number;
-  starInfo: StarInfo | null = null;
-  constructor(ctx: CanvasRenderingContext2D | null, width: number, height: number, starInfo?: StarInfo) {
-    this.ctx = ctx;
-    this.panelWidth = width;
-    this.panelHeight = height;
-    this.starInfo = starInfo ? starInfo : null;
+  private x: number;
+  private y: number;
+  private scale: number;
+  private alpha: number;
+  private speed: number;
+  private direction: number;
+
+  constructor(x: number, y: number, scale: number, alpha: number, speed: number, direction: number) {
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+    this.alpha = alpha;
+    this.speed = speed;
+    this.direction = direction;
   }
 
-  // 绘制星星到画布上
-  draw() {
-    if (!this.ctx) {
-      return;
-    }
-    if (this.starInfo) {
-      this.createStar(this.starInfo);
-    }
-
-    // 随机位置
-    const x = Math.random() * this.panelWidth;
-    const y = Math.random() * this.panelHeight;
-    // 随机大小
-    const scale = Math.random() * 2;
-    // 随机透明度
-    const alpha = Math.random();
-    // 黄色
-    this.ctx.fillStyle = `rgba(255, 255, 255, ${alpha})`;
-    this.ctx.beginPath();
-    this.ctx.arc(x, y, scale, 0, Math.PI * 2);
-    this.ctx.closePath();
-    this.ctx.fill();
-    return {
-      x,
-      y,
-      scale,
-      alpha
-    };
+  public draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.scale, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+    ctx.closePath();
+    ctx.fill();
   }
 
-  // 创建星星
-  createStar(starInfo: StarInfo) {
-    if (!this.ctx) {
-      return;
-    }
-    this.ctx.fillStyle = `rgba(255, 255, 255, ${starInfo.alpha})`;
-    this.ctx.beginPath();
-    this.ctx.arc(starInfo.x, starInfo.y, starInfo.scale, 0, Math.PI * 2);
-    this.ctx.closePath();
-    this.ctx.fill();
+  public move(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.x + this.speed, this.y + this.speed * this.direction, this.scale, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+    ctx.closePath();
+    ctx.fill();
 
-    return {
-      x: starInfo.x,
-      y: starInfo.y,
-      scale: starInfo.scale,
-      alpha: starInfo.alpha
-    };
+    this.x += this.speed;
+    this.y += this.speed * this.direction;
+    if (this.x > window.innerWidth || this.y > window.innerHeight) {
+      return true;
+    }
+  }
+}
+
+/**
+ * 流星类
+ */
+export class Meteor {
+  private x: number;
+  private y: number;
+  private scale: number = 5;
+  private alpha: number = 1;
+  private speed: number = 10;
+  private direction: number;
+
+  constructor(x: number, y: number, direction: number) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+  }
+
+  public draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.scale, 0, Math.PI * 2);
+    ctx.fillStyle = `red`;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  public move(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+    ctx.beginPath();
+    ctx.arc(this.x + this.speed, this.y + this.speed * this.direction, this.scale, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+    ctx.closePath();
+    ctx.fill();
+
+    this.x += this.speed;
+    this.y += this.speed * this.direction;
+    if (this.x > window.innerWidth || this.y > window.innerHeight) {
+      return true;
+    }
   }
 }

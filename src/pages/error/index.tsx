3e67cd9e417475f1b0476acb8d90f6a6abfe4eb6
LucausMartin <mzx2602685411@gmail.com>
2024-06-01 21:00:17 +0800

    feat: ✨ 完成 404 页面开发

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -11,6 +11,12 @@ const Error = () => {
     <div className="not-found">
       <div className="text">{language.LANGUAGE['404 Page Not Found']}</div>
       <div className="lamp">
+        <div className="light-line"></div>
+        <div className="lampshade"></div>
+        <div className="lampshade-oval-big"></div>
+        <div className="lampshade-oval-small-mask">
+          <div className="lampshade-oval-small"></div>
+        </div>
         <div className="light"></div>
       </div>
     </div>

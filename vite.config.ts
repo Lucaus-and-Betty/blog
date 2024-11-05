9adf1489ed848cdb65b9bbbc3a981c651064b202
betty <wusehun94zlj@gmail.com>
2024-11-05 18:05:45 +0800

    添加 ts 检查

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { join } from "path";
 import react from '@vitejs/plugin-react-swc'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
   resolve: {
@@ -16,7 +17,9 @@ export default defineConfig({
       '@': join(__dirname, 'src'),
     }
   },
-  plugins: [react()],
+  plugins: [react(), checker({
+    typescript: true
+  })],
   css: {
     preprocessorOptions: {
       less: {

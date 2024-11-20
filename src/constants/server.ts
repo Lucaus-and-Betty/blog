e6e5e1bfffc711dcd1c2c8f381895e78dfce214e
betty <wusehun94zlj@gmail.com>
2024-11-20 13:07:51 +0800

    feat: ✨ 添加环境变量

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,9 +1,9 @@
 /**
  * @description 服务器接口地址
  */
-export const SERVER_URL = '//localhost:3000/v1';
+export const SERVER_URL = import.meta.env.VITE_SERVER_HOST;
 
 /**
  * @description 服务器图片静态地址
  */
-export const SERVER_IMG_URL = '//localhost:3001/static/';
+export const SERVER_IMG_URL = import.meta.env.VITE_SERVER_IMG_URL;
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,30 @@
 import { defineConfig } from 'vite'
 import { join } from "path";
 import react from '@vitejs/plugin-react-swc'
-// import checker from 'vite-plugin-checker'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@myHooks': join(__dirname, 'src/hooks'),
-      '@myComponents': join(__dirname, 'src/components'),
-      '@myUtils': join(__dirname, 'src/utils'),
-      '@myStore': join(__dirname, 'src/store'),
-      '@myTypes': join(__dirname, 'src/types'),
-      '@myPages': join(__dirname, 'src/pages'),
-      '@myAssets': join(__dirname, 'src/assets'),
-      '@myConstants': join(__dirname, 'src/constants'),
-      '@': join(__dirname, 'src'),
-    }
-  },
-  plugins: [react(), 
-  //   checker({
-  //   typescript: true
-  // })
-],
-  css: {
-    preprocessorOptions: {
-      less: {
-        math: "always",
+
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        '@myHooks': join(__dirname, 'src/hooks'),
+        '@myComponents': join(__dirname, 'src/components'),
+        '@myUtils': join(__dirname, 'src/utils'),
+        '@myStore': join(__dirname, 'src/store'),
+        '@myTypes': join(__dirname, 'src/types'),
+        '@myPages': join(__dirname, 'src/pages'),
+        '@myAssets': join(__dirname, 'src/assets'),
+        '@myConstants': join(__dirname, 'src/constants'),
+        '@': join(__dirname, 'src'),
+      }
+    },
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        less: {
+          math: "always",
+        },
       },
     },
-  },
+  }
 })
